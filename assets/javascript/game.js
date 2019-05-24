f3ff91dbb5354d76780df1032d396d5b3444139a
Mitch Webster <mitch83083@gmail.com>
2019-05-24 14:26:18 -0400

    got game working in initial state

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,11 +1,11 @@
 var randomWords = ["dog", "cat", "bird", "horse"];
 var maxTries = 10;
 var guessedLetters = [];
-var currentWordIndex = [];
+var currentWordIndex = "";
 var guessingWord = [];
 var remainingGuesses = 0;
-var gameStarted = false;
-var gameFinished = false;
+// var gameStarted = false;
+// var gameFinished = false;
 var wins = 0;
 var userGuess = "";
 
@@ -17,14 +17,17 @@ function resetGame() {
   guessingWord = [];
   newWord();
   updateDisplay();
+  console.log(currentWordIndex);
 }
 
+// Picks new word splits it up and puts it in currentWordIndex
 function newWord() {
   currentWordIndex = Math.floor(Math.random() * randomWords.length);
   for (var i = 0; i < randomWords[currentWordIndex].length; i++) {
     guessingWord.push(" _");
   }
-  currentWordIndex = randomWords[currentWordIndex];
+  currentWordIndex = randomWords[currentWordIndex].split("");
+  return currentWordIndex;
 }
 
 // Refresh display
@@ -38,24 +41,51 @@ function updateDisplay() {
   document.getElementById("guessedLetters").innerText = guessedLetters;
 }
 
+// On keystroke
 document.onkeyup = function(event) {
-  userGuess = event.key;
+  userGuess = event.key.toLowerCase();
+  makeGuess(userGuess);
   console.log(userGuess);
-  search(currentWordIndex, userGuess, remainingGuesses);
 };
 
-function search(a, b, c) {
-  for (var i = 0; i < a.length; i++) {
-    if (a[i] === b) {
-      console.log("test");
-      return i;
-    }
-    updateDisplay();
-    console.log(updateDisplay);
+// Checks to see if letter has been guessed if not pushes to guessedLetters array
+function makeGuess(letter) {
+  if (guessedLetters.indexOf(letter) === -1) {
+    guessedLetters.push(letter);
+    search(letter);
   }
-  // return -1;
+  updateDisplay();
+  checkWin();
+}
+
+// Searchs through current word array to see if any matches
+// Creates new array to hold matches then pushes them
+// Had some trouble with this, had to look on the internet for some help.
+// I understand what is going on but it seems clunky and not the best way.
+function search(letter) {
+  var positions = [];
+  for (var i = 0; i < currentWordIndex.length; i++) {
+    if (currentWordIndex[i] === letter) {
+      positions.push(i);
+    }
+  }
+
+  if (positions.length <= 0) {
+    remainingGuesses--;
+  } else {
+    for (var i = 0; i < positions.length; i++) {
+      guessingWord[positions[i]] = letter;
+    }
+  }
+}
+
+// Checks for all _ to be gone and updates win / resets game.
+function checkWin() {
+  if (guessingWord.indexOf(" _") === -1) {
+    wins++;
+    // gameFinished = true;
+    resetGame();
+  }
 }
 
 resetGame();
-
-console.log(currentWordIndex);
